8030cae887083e3e9990b6fd8d18fdc8c9ca26d3
keerthi <keerthiraj.4801@gmail.com>
2023-12-17 22:03:12 +0530

    update postcard

--- src/components/Main/postcard.jsx
+++ src/components/Main/postcard.jsx
@@ -112,76 +112,82 @@ const PostCard = ({ uid, id, logo, name, email, text, image, timestamp }) => {
   }, [id, ADD_LIKE, HANDLE_ERROR]);
 
   return (
-    <div className="mb-4">
-      <div className="flex flex-col py-4 bg-white rounded-t-3xl">
-        <div className="flex justify-start items-center pb-4 pl-4 ">
-          <Avatar
-            size="sm"
-            variant="circular"
-            src={logo || avatar}
-            alt="avatar"
-          ></Avatar>
+    <div>
+      <div className="mb-4">
+        <div className="flex flex-col py-4 bg-white rounded-t-3xl">
+          <div className="flex justify-start items-center pb-4 pl-4 ">
+            <Avatar
+              size="sm"
+              variant="circular"
+              src={logo || avatar}
+              alt="avatar"
+            ></Avatar>
 
-          <div className="flex flex-col ml-4">
-            <p className=" py-2 font-roboto font-medium text-sm text-gray-700 no-underline tracking-normal leading-none">
-              {email}
-            </p>
-            <p className=" font-roboto font-medium text-sm text-gray-700 no-underline tracking-normal leading-none">
-              Published: {timestamp}
-            </p>
-          </div>
-          {user?.uid !== uid && (
-            <div
-              onClick={addUser}
-              className="w-full flex justify-end cursor-pointer mr-10"
-            >
-              <img
-                className="hover:bg-blue-100 rounded-xl p-2"
-                src={addFriend}
-                alt="addFriend"
-              ></img>
+            <div className="flex flex-col ml-4">
+              <p className=" py-2 font-roboto font-medium text-sm text-gray-700 no-underline tracking-normal leading-none">
+                {email}
+              </p>
+              <p className=" font-roboto font-medium text-sm text-gray-700 no-underline tracking-normal leading-none">
+                Published: {timestamp}
+              </p>
             </div>
-          )}
-        </div>
-        <div>
-          <p className="ml-4 pb-4 font-roboto font-medium text-sm text-gray-700 no-underline tracking-normal leading-none">
-            {text}
-          </p>
-          {image && (
-            <img className="h-[500px] w-full" src={image} alt="postImage"></img>
-          )}
-        </div>
-        <div className="flex justify-around items-center pt-4">
-          <button
-            className="flex items-center cursor-pointer rounded-lg p-2 hover:bg-gray-100"
-            onClick={handleLike}
-          >
-            <img className="h-8 mr-4" src={like} alt=""></img>
-            {state.likes?.length > 0 && state?.likes?.length}
-          </button>
-          <div
-            className="flex items-center cursor-pointer rounded-lg p-2 hover:bg-gray-100"
-            onClick={handleOpen}
-          >
-            <div className="flex items-center cursor-pointer">
-              <img className="h-8 mr-4" src={comment} alt="comment"></img>
+            {user?.uid !== uid && (
+              <div
+                onClick={addUser}
+                className="w-full flex justify-end cursor-pointer mr-10"
+              >
+                <img
+                  className="hover:bg-blue-100 rounded-xl p-2"
+                  src={addFriend}
+                  alt="addFriend"
+                ></img>
+              </div>
+            )}
+          </div>
+          <div>
+            <p className="ml-4 pb-4 font-roboto font-medium text-sm text-gray-700 no-underline tracking-normal leading-none">
+              {text}
+            </p>
+            {image && (
+              <img
+                className="h-[500px] w-full"
+                src={image}
+                alt="postImage"
+              ></img>
+            )}
+          </div>
+          <div className="flex justify-around items-center pt-4">
+            <button
+              className="flex items-center cursor-pointer rounded-lg p-2 hover:bg-gray-100"
+              onClick={handleLike}
+            >
+              <img className="h-8 mr-4" src={like} alt=""></img>
+              {state.likes?.length > 0 && state?.likes?.length}
+            </button>
+            <div
+              className="flex items-center cursor-pointer rounded-lg p-2 hover:bg-gray-100"
+              onClick={handleOpen}
+            >
+              <div className="flex items-center cursor-pointer">
+                <img className="h-8 mr-4" src={comment} alt="comment"></img>
+                <p className="font-roboto font-medium text-md text-gray-700 no-underline tracking-normal leading-none">
+                  Comments
+                </p>
+              </div>
+            </div>
+            <div
+              className="flex items-center cursor-pointer rounded-lg p-2 hover:bg-gray-100"
+              onClick={deletePost}
+            >
+              <img className="h-8 mr-4" src={remove} alt="delete"></img>
               <p className="font-roboto font-medium text-md text-gray-700 no-underline tracking-normal leading-none">
-                Comments
+                Delete
               </p>
             </div>
           </div>
-          <div
-            className="flex items-center cursor-pointer rounded-lg p-2 hover:bg-gray-100"
-            onClick={deletePost}
-          >
-            <img className="h-8 mr-4" src={remove} alt="delete"></img>
-            <p className="font-roboto font-medium text-md text-gray-700 no-underline tracking-normal leading-none">
-              Delete
-            </p>
-          </div>
         </div>
+        {open && <CommentSection postId={id}></CommentSection>}
       </div>
-      {open && <CommentSection postId={id}></CommentSection>}
     </div>
   );
 };
